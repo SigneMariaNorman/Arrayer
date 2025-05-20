88270d3341b26361470eacf715dc97422bfd97af
Maria Norman <signemaria83@gmail.com>
2025-05-20 15:32:10 +0200

    Fixat små fel

--- Index.js
+++ Index.js
@@ -3,9 +3,9 @@
 * Det går att blanda olika datatyper i en array men det rekommenderas ej.
 */
 let myArray = []; //hak-parenteser [] visar att det handlar om 'Array'
-let pokemons = ["Charmander", "Bulbasaur", "Squirtle"];
+let pokemons = ['Charmander', 'Bulbasaur', 'Squirtle'];
 let numbers = [1, 2, 3, 4, 5];
-let mix = ['Pikachu', 1, 2, "Charmander", true];
+let mix = ['Pikachu', 1, 2, 'Charmander', true];
 /*Indexering
 * När man hämtar en Array säger man att man indexerar.
 * Första värdet i listan har index 0 och sista värdet har indexlängden-1.
@@ -13,9 +13,9 @@ let mix = ['Pikachu', 1, 2, "Charmander", true];
 * Försöker man komma åt ett element utanför sin Array får man "undefined".
 */
 
-pokemons.length -> 4
-pokemons[0]; -> 'Pikachu'
-pokemons[pokemons.length - 1]; -> 'Squirtle'
+pokemons.length // 3, eftersom pokemons är inom [] så betyder det att det handlar om arrays, pch då räknar length ut antal element, inte tecken.
+pokemons[0]; // 'Charmander'
+pokemons[pokemons.length - 1]; // -1 visar det sista elementet i arrayen. Alltså Squirtle i detta fall. 
 
 
 /* PROPERTIES
@@ -25,4 +25,8 @@ pokemons[pokemons.length - 1]; -> 'Squirtle'
 
 *METODER:
 *Finns för ett antal fördefinierade metoder för arrayer.
-* Några av dessa är: push(), pop(), indexOf().
+* Några av dessa är: push(), pop(), indexOf(). */
+
+pokemons.push('Pikachu'); // Lägger till 'Pikachu' i slutet av arrayen pokemons.
+let pokemon = pokemons.pop(); //tar bort det sista elementet i arrayen och sparar det i variabeln pokemon.
+let index = pokemons.indexOf('Bulbasaur'); // Försöker hitta index för 'Bulbasaur' i arrayen. 
